7a708c3d7f777e2d301c2c8d416b29a20e794eab
mrtnvgr <martynovegorOF@yandex.ru>
2022-08-01 14:29:18 +0700

    v0.0.0-1 release

---
+++ main.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+from vk_api.longpoll import VkEventType, VkLongPoll
+from vk_api.utils import get_random_id
+import vk_api, json, time, re
+
+class Main:
+    def __init__(self):
+        self.version = "0.0.0-1"
+        self.reloadConfig()
+        self.initVkApi()
+        self.listen()
+
+    def reloadConfig(self):
+        self.config = json.load(open("config.json"))
+        if "mutedUsers" not in self.config:
+            self.config["mutedUsers"] = []
+
+    def saveConfig(self):
+        json.dump(self.config, fp=open("config.json", "w"), indent=4)
+
+    def initVkApi(self):
+        self.vk = vk_api.VkApi(token=self.config["token"])
+        self.longpoll = VkLongPoll(self.vk)
+
+    def sendreply(self, event, text, reply=True):
+        payload = {"chat_id": event.chat_id, "random_id": get_random_id(), "message": text}
+        if reply:
+            payload["reply_to"] = event.message_id
+        self.method("messages.send", payload)
+
+    def sendme(self, event, text):
+        payload = {"user_id": event.user_id, "random_id": get_random_id(), "message": text}
+        self.method("messages.send", payload)
+
+    def listen(self):
+        for event in self.longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW: # TODO: message edit
+                if event.from_chat:
+                    self.eventHandler(event)
+
+    def eventHandler(self, event):
+        self.restrictionsHandler(event)
+        self.cmdHandler(event)
+        # NOTE: self.animebanHandler()...
+
+    def cmdHandler(self, event):
+        if hasattr(event, "text"):
+            if event.from_me:
+                self.muteHandler(event)
+                self.unMuteHandler(event)
+                self.helpHandler(event)
+                # NOTE: self.unmuteHandler(event)...
+
+    def muteHandler(self, event):
+        text = event.text.split(" ")
+        if text[0] in ("!мут", "!молчи", "!помолчи", "!молчать",
+                       "!терпи", "!потерпи", "!завали", "!заткнись"):
+            if len(text)>1:
+                user_id, user_name = self.getmentioninfo(event)
+                # NOTE: implement mute all
+                # TODO: make independent function for getting user_id and user_name
+                if len(text)==3:
+                    time = text[2]
+                    text[2] = self.gettime(text[2])
+                else:
+                    text.append(-1)
+                    time = text[2]
+                self.config["mutedUsers"].append([user_id, event.chat_id, text[2]])
+                self.saveConfig()
+                self.sendreply(event, f"{user_name} замучен на {time}.")
+            else:
+                pass # TODO: all
+
+    def unMuteHandler(self, event):
+        text = event.text.split(" ")
+        if text[0] in ("!размут", "!анмут"):
+            if len(text)>1:
+                user_id, user_name = self.getmentioninfo(event)
+                for i,user in enumerate(self.config["mutedUsers"]):
+                    if user[0]==user_id and user[1]==event.chat_id:
+                        self.config["mutedUsers"].pop(i)
+                        self.sendreply(event, f"{user_name} размучен.")
+                        self.saveConfig()
+                        break
+            else:
+                self.config["mutedUsers"] = []
+                self.saveConfig()
+                self.sendreply(event, "Все размучены.")
+
+    def helpHandler(self, event):
+        text = event.text.split(" ")
+        if text[0] in ("!хелп", "!help", "!помощь", "!справка"):
+            self.sendme(event, self.gethelptext())
+
+    def restrictionsHandler(self, event):
+        self.mutedUserHandler(event)
+
+    def mutedUserHandler(self, event):
+        for user in self.config["mutedUsers"]:
+            if int(time.time())>=user[2] and user[2]!=-1:
+                self.config["mutedUsers"].remove(user)
+            else:
+                if user[0]==event.user_id and event.chat_id==user[1]:
+                    self.method("messages.delete", {"message_ids": event.message_id, "delete_for_all": 1})
+
+    @staticmethod
+    def gettime(st):
+        cur = int(time.time())
+        num = re.findall(r"\d+", st)[0]
+        st = st.removeprefix(num)
+        if st in ("м","мин"):
+            num *= 60
+        elif st=="ч":
+            num *= 3600
+        elif st=="д":
+            num *= 86400
+        return cur+int(num)
+
+    @staticmethod
+    def getmentioninfo(event):
+        splitted = event.text.split(" ")[1].split("|")
+        user_id = int(splitted[0].removeprefix("[id"))
+        user_name = splitted[1].removesuffix("]")
+        return user_id, user_name
+
+    def gethelptext(self):
+        text = []
+        text.append(f"VKPlus v{self.version} (github.com/mrtnvgr/vkplus)")
+        text.append("Команды:")
+        text.append("   !мут (!молчи, !помолчи, !молчать, !терпи, !потерпи, !завали, !заткнись) - мут")
+        text.append("   !анмут (!размут) - анмут")
+        text.append("   !помощь (!хелп, !help, !справка) - справка в избранное")
+        return "\n".join(text)
+
+    def method(self, *args, **kwargs):
+        self.vk.method(*args, **kwargs)
+
+if __name__=="__main__":
+    Main()
