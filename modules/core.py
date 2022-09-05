1118feec376f6b4c2e30651bc4a2d47a485593f1
mrtnvgr <martynovegorOF@yandex.ru>
2022-09-05 21:18:29 +0700

    optimizations?

--- modules/core.py
+++ modules/core.py
@@ -87,11 +87,11 @@ class CoreModule:
 
 def speed_change(url, speed=1.0):
     file = urllib.request.urlopen(url).read()
-    sound = AudioSegment.from_file(BytesIO(file))
+    sound = AudioSegment.from_mp3(BytesIO(file))
     sound2 = sound._spawn(sound.raw_data, overrides={
          "frame_rate": int(sound.frame_rate * speed)
       })
     sound2 = sound2.set_frame_rate(sound.frame_rate)
     buf = BytesIO()
-    sound2.export(buf)
+    sound2.export(buf, format="mp3")
     return buf.getvalue()
