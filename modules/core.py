39286c781e7c95c272fca34624c761b8f2b6d9dc
mrtnvgr <martynovegorOF@yandex.ru>
2022-09-07 21:37:49 +0700

    core cmd: cleaning

--- modules/core.py
+++ modules/core.py
@@ -4,104 +4,132 @@ import urllib, json
 class CoreModule:
     def __init__(self, master):
         self.master = master
+        self.reset()
         self.speed = {"nc": "1.35",
                       "snc": "1.17",
 
                       "dc": "0.70",
                       "sdc": "0.85"}
 
+    def reset(self):
+        self.stuff = {}
+        self.objs = {}
+
     def getCoreType(self, event):
-        default = True
-        typecore = None
+        self.typecore_default = True
+        self.typecore = None
         if len(event.text)==1:
             if event.text[0] in ("nightcore", "nc", "нк",
                                  "найткор", "найткоре"):
                 event.text.append(self.speed["nc"])
-                typecore = "nightcore"
+                self.typecore = "nightcore"
             elif event.text[0] in ("softnightcore", "snightcore",
                                    "softnc", "snc", "снк",
                                    "софтнайткор", "софтнайткоре"):
                 event.text.append(self.speed["snc"])
-                typecore = "soft nightcore"
+                self.typecore = "soft nightcore"
             elif event.text[0] in ("daycore", "dc", "дк",
                                    "дейкор", "дэйкор",
                                    "дейкоре", "дэйкоре"):
                 event.text.append(self.speed["dc"])
-                typecore = "daycore"
+                self.typecore = "daycore"
             elif event.text[0] in ("softdaycore", "sdaycore",
                                    "softdc", "sdc", "сдк",
                                    "софтдейкор", "софтдэйкор",
                                    "софтдейкоре", "софтдэйкоре"):
                 event.text.append(self.speed["sdc"])
-                typecore = "soft daycore"
+                self.typecore = "soft daycore"
             elif event.text[0] in ("core", "коре", "кор"):
                 event.text.append(self.speed["nc"])
-                typecore = "nightcore"
+                self.typecore = "nightcore"
             else:
-                default = False
+                self.typecore_default = False
         else:
             if not event.text[1].replace(".","",1).isdigit():
                 return
             speed = float(event.text[1])
             if speed<=float(self.speed["dc"]):
-                typecore = "daycore"
+                self.typecore = "daycore"
             elif speed<=float(self.speed["sdc"]):
-                typecore = "soft daycore"
+                self.typecore = "soft daycore"
             elif speed<=float(self.speed["snc"]):
-                typecore = "soft nightcore"
+                self.typecore = "soft nightcore"
             elif speed<=float(self.speed["nc"]):
-                typecore = "nightcore"
-        return typecore, default
+                self.typecore = "nightcore"
 
     def coreHandler(self, event):
-        typecore, default = self.getCoreType(event)
         if event.user_id in self.master.config["perms"]["core"] or event.from_me:
+            self.getCoreType(event)
             if event.attachments!={}:
                 if not event.from_me:
                     if float(event.text[1])<0.50 or float(event.text[1])>1.50:
                         self.master.sendreply(event, "Ограничения скорости 0.5-1.5")
                         return
-                audios = []
-                for i in range(len(event.attachments)):
-                    if event.attachments.get(f"attach{i+1}_type","")=="audio":
-                        audio = event.attachments[f"attach{i+1}"]
-                        if len(audio.split("_"))==2:
-                            response = self.master.method("audio.getById", 
-                                                         {"audios": audio})
-                            if type(response) is int:
-                                self.master.sendreply(event, "Не удалось получить аудиозапись.")
-                                return
-                            else:
-                                if "access_key" in response:
-                                    audio += f"_{response['access_key']}"
-                        audios.append(audio)
-                if "reply" in event.attachments:
-                    ids = json.loads(event.attachments["reply"])["conversation_message_id"]
-                    response = self.master.method("messages.getByConversationMessageId",
-                                           {"peer_id": event.peer_id,
-                                            "conversation_message_ids": ids})
-                    for item in response["items"]:
-                        for attachment in item["attachments"]:
-                            if attachment["type"]=="audio":
-                                audio = attachment["audio"]
-                                audios.append(f"{audio['owner_id']}_{audio['id']}_{audio['access_key']}")
-                response = self.master.method("audio.getById",
-                                      {"audios": ",".join(audios)})
-                if audios==[]: return
-                attachments = []
-                for audio in response:
-                    if not event.from_me:
-                        if audio["duration"]>300:
-                            self.master.sendreply(event, "Ограничения времени 0-300")
-                            return
-                    data = speed_change(audio["url"],
-                                                float(event.text[1]))
-                    artist = "ᵖ¹³ᵈ³ᶻ"
-                    title = f'{audio["title"]} +| {typecore}'
-                    if not default: title += f" x{event.text[1]}"
-                    newAudio = self.master.uploadAudio(data, artist, title)
-                    attachments.append(f"audio{newAudio['owner_id']}_{newAudio['id']}_{newAudio['access_key']}")
+                self.reset()
+                self.getAudios(event)
+                self.getReplyAttachments(event)
+                if self.stuff=={}: return
+                self.parseAudios(event)
+                if self.objs=={}: return
+                attachments = self.uploadObjs(event)
                 self.master.sendreply(event, None, attachments)
+
+    def getAudios(self, event):
+        for i in range(len(event.attachments)):
+            if event.attachments.get(f"attach{i+1}_type","")=="audio":
+                audio = event.attachments[f"attach{i+1}"]
+                if len(audio.split("_"))==2:
+                    response = self.master.method("audio.getById", 
+                                                 {"audios": audio})
+                    if type(response) is int:
+                        self.master.sendreply(event, "Не удалось получить аудиозапись.")
+                        continue
+                    else:
+                        if "access_key" in response:
+                            audio += f"_{response['access_key']}"
+                self.stuff = self.add(self.stuff, "audio", audio)
+
+    def getReplyAttachments(self, event):
+        if "reply" in event.attachments:
+            ids = json.loads(event.attachments["reply"])["conversation_message_id"]
+            response = self.master.method("messages.getByConversationMessageId",
+                                   {"peer_id": event.peer_id,
+                                    "conversation_message_ids": ids})
+            for item in response["items"]:
+                for attachment in item["attachments"]:
+                    if attachment["type"]=="audio":
+                        audio = attachment["audio"]
+                        self.stuff = self.add(self.stuff, "audio", f"{audio['owner_id']}_{audio['id']}_{audio['access_key']}")
+
+    def parseAudios(self, event):
+        response = self.master.method("audio.getById",
+                                     {"audios": ",".join(self.stuff["audio"])})
+        for audio in response:
+            if not event.from_me:
+                if audio["duration"]>300:
+                    self.master.sendreply(event, "Ограничения времени 0-300")
+                    continue
+            self.objs = self.add(self.objs, "audio", audio)
+
+    def uploadObjs(self, event):
+        attachments = []
+        artist = "ᵖ¹³ᵈ³ᶻ"
+        for name in self.objs:
+            for obj in self.objs[name]:
+                if name=="audio":
+                    data = speed_change(obj["url"],
+                                        float(event.text[1]))
+                    title = f'{obj["title"]} +| {self.typecore}'
+                new = self.master.uploadAudio(data, artist, title)
+                if not self.typecore_default: title += f" x{event.text[1]}"
+                attachments.append(f"{name}{new['owner_id']}_{new['id']}_{new['access_key']}")
+        return attachments
+
+    def add(self, where, group, data):
+        if group not in where:
+            where[group] = []
+        where[group].append(data)
+        return where
 
 def speed_change(url, speed=1.0):
     file = urllib.request.urlopen(url).read()
